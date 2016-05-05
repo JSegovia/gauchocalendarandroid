ab9ec394d9af4b61ad2f30f45b133d69f3df9c87
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-05-05 15:37:40 -0700

    gets to login, can't go to fb

--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/StudentLogin.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/StudentLogin.java
@@ -1,26 +1,57 @@
 package com.ucsbstudent.phillip.gauchocalendar_android;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.firebase.client.core.view.View;
+import com.firebase.client.Firebase;
 
 /**
  * Created by Phillip on 5/5/2016.
  */
-public class StudentLogin extends AppCompatActivity {
+public class  StudentLogin extends AppCompatActivity {
+private static final String FIREBASE_URL="https://sizzling-inferno-7789.firebaseIO.com";
+private Firebase firebaseRef;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+        Firebase.setAndroidContext(this);
+        firebaseRef = new Firebase(FIREBASE_URL);
+
+        findViewById(R.id.LoginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username = (EditText) findViewById(R.id.TextFieldUSERNAME);
+                EditText password = (EditText) findViewById(R.id.TextFieldPASSWORD);
+                String user_name = username.getText().toString();
+                String pass_word = password.getText().toString();
+                String email = user_name + "@umail.ucsb.edu";
+
+                if (user_name != "" && pass_word != "") {
+                    StudentProfile student = new StudentProfile(user_name, pass_word, email);
+                    firebaseRef.push().setValue(student);
+                    username.setText("");
+                    password.setText("");
+                    Toast.makeText(StudentLogin.this, "Account Inserted", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
-    public void buttonSignUp(View view){
+
+
+/*
+    public void buttonSignUp(com.firebase.client.core.view.View view){
         Intent intent = new Intent(this, Information.class);
         startActivity(intent);
 
 
     }
-
+*/
 }
