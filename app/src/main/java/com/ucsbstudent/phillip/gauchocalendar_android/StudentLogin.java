d41cc7659acba3c2917b9eac3c94f891e7775968
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-05-05 12:51:24 -0700

    should run

--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/StudentLogin.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/StudentLogin.java
@@ -1,7 +1,26 @@
 package com.ucsbstudent.phillip.gauchocalendar_android;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.client.core.view.View;
+
 /**
  * Created by Phillip on 5/5/2016.
  */
-public class StudentLogin {
+public class StudentLogin extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+    public void buttonSignUp(View view){
+        Intent intent = new Intent(this, Information.class);
+        startActivity(intent);
+
+
+    }
+
 }
