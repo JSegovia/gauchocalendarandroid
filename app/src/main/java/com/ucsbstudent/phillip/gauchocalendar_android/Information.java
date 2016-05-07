7473df4d3019a061cb3856d14d4bd43afaf39896
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-05-07 00:20:25 -0700

    Getting GUI going

--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/Information.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/Information.java
@@ -1,8 +1,10 @@
 package com.ucsbstudent.phillip.gauchocalendar_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Information extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+    }
+
+    public void start(View view){
+        Intent intent = new Intent(this, InputEvents.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/InputEvents.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/InputEvents.java
@@ -1,8 +1,12 @@
 package com.ucsbstudent.phillip.gauchocalendar_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 public class InputEvents extends AppCompatActivity {
 
     @Override
@@ -10,4 +14,22 @@ public class InputEvents extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_events);
     }
+/*
+    Spinner dropdown = (Spinner)findViewById(R.id.spinnersubject);
+    String[] subjects = new String[]{"ANTH", "ART", "ART CS", "ARTHI", "ARTST", "AS AM"};
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, subjects);
+
+    public void setAdapter(ArrayAdapter<String> adapter) {
+        this.adapter = adapter;
+    }
+*/
+    public void goCalendar(View view){
+        Intent intent = new Intent(this, CalendarView.class);
+        startActivity(intent);
+
+
+    }
+
+
+
 }
