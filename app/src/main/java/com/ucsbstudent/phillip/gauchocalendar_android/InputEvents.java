2a45f783dd92e7141aa6ca18c38f4aacb794848b
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-05-29 03:29:42 -0700

    Edit on insert items activity

--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/InputEvents.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/InputEvents.java
@@ -22,6 +22,8 @@ public class InputEvents extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_events);
 
+
+        // Spinner for Classes
         spinner = (Spinner) findViewById(R.id.spinnersubject);
         adapter = ArrayAdapter.createFromResource(this,R.array.departments, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -32,13 +34,13 @@ public class InputEvents extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
                 Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent){
-
             }
         });
 
+
+        //Spinner for quarter
         spinner = (Spinner) findViewById(R.id.spinnerQuarter);
         adapter = ArrayAdapter.createFromResource(this,R.array.Quarter,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -48,10 +50,68 @@ public class InputEvents extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " selected",Toast.LENGTH_LONG).show();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
+        //Spinner for Weekday
+        spinner = (Spinner) findViewById(R.id.spinnerweekday);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Weekday,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Spinner for Hours
+        spinner = (Spinner) findViewById(R.id.spinnerhour);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Hours,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Spinner for Minutes
+        spinner = (Spinner) findViewById(R.id.spinnermin);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Min,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Spinner for AMPM
+        spinner = (Spinner) findViewById(R.id.spinnerAmPm);
+        adapter = ArrayAdapter.createFromResource(this,R.array.AmPm,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
     }
