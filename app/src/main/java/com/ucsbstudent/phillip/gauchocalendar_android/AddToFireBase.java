1e15d59f2eba46381a3967e1f76869fa0cc3caa2
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-05-30 02:20:33 -0700

    Added activity for adding data to database

---
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/AddToFireBase.java
@@ -0,0 +1,13 @@
+package com.ucsbstudent.phillip.gauchocalendar_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddToFireBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_fire_base);
+    }
+}
