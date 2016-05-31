f63866f0afbda4c57629601cd16ebd2b5846b5a6
christiannewkirk <christiannewkirk@gmail.com>
2016-05-30 21:48:31 -0700

    Added parsedcourses

--- app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/AddToFireBase.java
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/AddToFireBase.java
@@ -1,8 +1,11 @@
 package com.ucsbstudent.phillip.gauchocalendar_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class AddToFireBase extends AppCompatActivity {
@@ -12,5 +15,22 @@ public class AddToFireBase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_fire_base);
+
+        ArrayList<Lecture> newCourses= new ArrayList<Lecture>();
+        try
+        {
+            FileInputStream fis = new FileInputStream("/Users/christiannewkirk/AndroidStudioProjects/gauchocalendarandroid/app/src/main/res/raw/parsedcourses.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            newCourses = (ArrayList) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+            return;
+        }catch(ClassNotFoundException c){
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
     }
 }
