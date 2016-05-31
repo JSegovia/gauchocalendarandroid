6467ea6c49743f1563232a26a009cb82c7387fbd
christiannewkirk <christiannewkirk@gmail.com>
2016-05-30 20:59:29 -0700

    Added Section and Lecture Java files

---
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/Lecture.java
@@ -0,0 +1,64 @@
+package com.ucsbstudent.phillip.gauchocalendar_android;
+
+import java.util.ArrayList;
+
+/**
+ * Created by christiannewkirk on 5/30/16.
+ */
+public class Lecture {
+
+        String name;
+        String days;
+        String time;
+        String occupancy;
+        String location;
+
+
+        public ArrayList<Section> sections = new ArrayList<Section>();
+
+        Lecture(String name, String lectureDays, String lectTime, String location, String occupancy){
+
+            this.name = name;
+            this.days = lectureDays;
+            this.time = lectTime;
+            this.location = location;
+            this.occupancy = occupancy;
+
+        }
+
+        void addSection(String day, String time, String location, String occupancy){
+            Section newSection = new Section(day, time, location, occupancy);
+
+            sections.add(newSection);
+        }
+
+        String getName(){
+            return this.name;
+        }
+        String getDays(){
+            return this.days;
+        }
+        String getTime(){
+            return this.time;
+        }
+        String getLocation(){
+            return this.location;
+        }
+        String getOcc(){
+            return this.occupancy;
+        }
+
+        void print(){
+            System.out.println(name);
+            System.out.println(days);
+            System.out.println(time);
+            System.out.println(occupancy);
+
+            for(int i = 0; i < sections.size(); i++){
+                sections.get(i).print();
+            }
+        }
+
+
+
+}
---
+++ app/src/main/java/com/ucsbstudent/phillip/gauchocalendar_android/Section.java
@@ -0,0 +1,38 @@
+package com.ucsbstudent.phillip.gauchocalendar_android;
+/**
+ * Created by christiannewkirk on 5/30/16.
+ */
+
+public class Section{
+
+    String day;
+    String time;
+    String occupancy;
+    String location;
+
+    Section(String day, String time, String location, String occupancy){
+        this.day = day;
+        this.time = time;
+        this.location = location;
+        this.occupancy = occupancy;
+
+    }
+
+    String getDays(){
+        return this.day;
+    }
+    String getTime(){
+        return this.time;
+    }
+    String getLocation(){
+        return this.location;
+    }
+    String getOcc(){
+        return this.occupancy;
+    }
+    void print(){
+        System.out.println(day);
+        System.out.println(time);
+        System.out.println(occupancy);
+    }
+}
